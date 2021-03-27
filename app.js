2bcf51f6beb034a5ce1ea8c80a89fa9b626eccda
Marc319-jr <marcelo.sznek@gmail.com>
2021-03-27 03:09:21 -0300

    primera noche de trabajo

---
+++ app.js
@@ -0,0 +1,18 @@
+const server =  3000;
+const express = require('express');
+const { dirname } = require('path');
+const app = express();
+const path = require ('path');
+
+app.use(express.static('public'));
+
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+});
+
+app.get('/test' , (req,res) => {
+    res.sendFile(path.join(__dirname ,'/views/pruebas.html'))
+});
+
+app.listen(server , () => console.log("levantando un servidor"));
